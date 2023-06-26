f38d1c2829492ff660778acf78618f85263ba783
Sameeranjankar27 <sameeranjankar8818@gmail.com>
2023-06-26 17:31:14 +0530

    first commit

--- House_price/setup.py
+++ House_price/setup.py
@@ -1,15 +1,25 @@
 from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
 
 
-
-
-
-
-setup(name="regression_project",
-      version='0.0.1'
-      author='sameer'
-      athor_mail='sameermaheshanjankar@gmail.com'
-      install_requires=['numpy','pandas'],
-      pakages=find_packages()  
-        
-        )
+setup(
+    name='House_price',
+    version='0.0.1',
+    author='sameer',
+    author_email='Sameeranjankar8818@gmail.com',
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
+)
