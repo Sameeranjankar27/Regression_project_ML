6a80147aa5a64e8854309e916abafed9b8599711
Sameeranjankar27 <sameeranjankar8818@gmail.com>
2023-06-26 16:11:26 +0530

    first commit

---
+++ House_price/README.md
@@ -0,0 +1,9 @@
+## end to end ml project
+## created an enviroment
+``` conda create -p venv python==3.8
+    conda activate venv/
+```
+### Install all lab
+```
+pip install -r requirements.txt
+```
---
+++ House_price/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages,setup
+
+
+
+
+
+
+setup(name="regression_project",
+      version='0.0.1'
+      author='sameer'
+      athor_mail='sameermaheshanjankar@gmail.com'
+      install_requires=['numpy','pandas'],
+      pakages=find_packages()  
+        
+        )
